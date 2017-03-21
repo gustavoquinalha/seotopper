80d74210101cd961bf0ecfa771a3b735667e83c9
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-03-21 12:01:59 -0300

    -q

--- js/index.controller.js
+++ js/index.controller.js
@@ -2,10 +2,40 @@ angular
     .module('app')
     .controller('IndexController', function($scope, Seo) {
     $scope.seo = {};
+    $scope.sv = true;
     $scope.salvar = function(seo) {
         Seo.salvar($scope.seo);
         $scope.seo = {};
         console.log("LOG PROD ======"+JSON.stringify(Seo.getSeos(), null, 3)+"======");
+    }
+
+    $scope.remover = function(x) {
+        Seo.remover(x);
+    }
+
+    $scope.ind = 0;
+    $scope.editor = false;
+    $scope.editando = function(x) {
+        $scope.editor = true;
+        $scope.sv = false;
+        $scope.seo.title = $scope.seos[$scope.seos.indexOf(x)].title;
+        $scope.seo.description = $scope.seos[$scope.seos.indexOf(x)].description;
+        $scope.seo.author = $scope.seos[$scope.seos.indexOf(x)].author;
+        $scope.seo.image = $scope.seos[$scope.seos.indexOf(x)].image;
+        $scope.seo.canonical = $scope.seos[$scope.seos.indexOf(x)].canonical;
+        $scope.seo.robots = $scope.seos[$scope.seos.indexOf(x)].robots;
+        $scope.seo.name = $scope.seos[$scope.seos.indexOf(x)].name;
+        $scope.seo.type = $scope.seos[$scope.seos.indexOf(x)].type;
+        $scope.seo.appid = $scope.seos[$scope.seos.indexOf(x)].appid;
+        $scope.seo.fbadmins = $scope.seos[$scope.seos.indexOf(x)].fbadmins;
+        $scope.seo.ttcard = $scope.seos[$scope.seos.indexOf(x)].ttcard;
+        $scope.ind = $scope.seos.indexOf(x);
+    }
+    $scope.editar = function(x) {
+        Seo.editar(x, $scope.ind);
+        $scope.editor = false;
+        $scope.sv = true;
+        $scope.seo = {};
     }
 
     $scope.seos = Seo.seos;
