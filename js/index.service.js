b911ceae6fd95b2fe5e18b38a65fab70812fd7ac
Japa <willian.hsimabukuro@gmail.com>
2017-03-20 22:37:57 -0300

    Add Edit and Remove

--- js/index.service.js
+++ js/index.service.js
@@ -5,8 +5,10 @@ angular
     var Seo = {
         salvar: salvar,
         getSeos: getSeos,
+        remover: remover,
+        editar: editar,
         seos: [
-          /*{
+          {
               id: 0,
               name: "teste",
               title: "titulo da minha pagina",
@@ -19,9 +21,9 @@ angular
               type: "website",
               appid: 12321312323,
               ttcard: "summary"
-          }*/
+          }
         ],
-        id: 0
+        id: 1
     };
     return Seo;
 
@@ -43,12 +45,32 @@ angular
             }
         );
         Seo.id++;
-        console.log(JSON.stringify(seo, null, 3)+" --Inserido--> "+JSON.stringify(Seo.seos, null, 3)+"===============");
+        console.log(JSON.stringify(seo, null, 12)+" --Inserido--> "+JSON.stringify(Seo.seos, null, 12)+"===============");
     }
 
     function getSeos(x) {
-        console.log("LOG LISTA ====== "+JSON.stringify(Seo.seos, null, 3)+" ===============");
+        console.log("LOG LISTA ====== "+JSON.stringify(Seo.seos, null, 12)+" ===============");
         return Seo.seos;
+    }
+    
+    function remover(x) {
+        Seo.seos.splice(Seo.seos.indexOf(x),1);
+        console.log("LOG REMOVER ====== "+JSON.stringify(Seo.seos, null, 12)+" ===============");
+    }
+    
+    function editar(x,n) {
+        Seo.seos[n].title = x.title;
+        Seo.seos[n].description = x.description;
+        Seo.seos[n].author = x.author;
+        Seo.seos[n].image = x.image;
+        Seo.seos[n].canonical = x.canonical;
+        Seo.seos[n].robots = x.robots;
+        Seo.seos[n].name = x.name;
+        Seo.seos[n].type = x.type;
+        Seo.seos[n].appid = x.appid;  
+        Seo.seos[n].fbadmins = x.fbadmins;
+        Seo.seos[n].ttcard = x.ttcard;
+        console.log("LOG EDIT ========= "+JSON.stringify(Seo.seos, null, 12));
     }
 
 });
