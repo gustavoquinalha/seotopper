b41f60c289c35ae77c51a3ad64f9b4ac602ac9c4
Gustavo Quinalha <gustavo_quinalha@hotmail.com>
2024-07-05 04:18:14 -0300

    -q

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,36 +86,36 @@ export class AppComponent {
   onChanges(): void {
     this.form.valueChanges.subscribe(val => {
       this.code = `
-      <!-- HTML Meta Tags -->
-      <meta charset="utf-8">
-      <base href="/">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>${this.form.get('title')?.value}</title>
-      <meta name="description" content="${this.form.get('description')?.value}">
-      <meta name="author" content="${this.form.get('author')?.value}">
-      <meta name="theme-color" content="${this.form.get('color')?.value}"/>
-      <link rel="icon" type="image/x-icon" href="${this.form.get('favicon')?.value}">
+  <!-- HTML Meta Tags -->
+  <meta charset="utf-8">
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>${this.form.get('title')?.value}</title>
+  <meta name="description" content="${this.form.get('description')?.value}">
+  <meta name="author" content="${this.form.get('author')?.value}">
+  <meta name="theme-color" content="${this.form.get('color')?.value}"/>
+  <link rel="icon" type="image/x-icon" href="${this.form.get('favicon')?.value}">
 
-      <!-- Facebook Meta Tags -->
-      <meta property="og:url" content="${this.form.get('url')?.value}">
-      <meta property="og:type" content="website">
-      <meta property="og:title" content="${this.form.get('title')?.value}">
-      <meta property="og:description" content="${this.form.get('description')?.value}">
-      <meta property="og:image" content="${this.form.get('image')?.value}">
-      <meta property="og:locale" content="${this.form.get('locale')?.value}"/>
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="${this.form.get('url')?.value}">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="${this.form.get('title')?.value}">
+  <meta property="og:description" content="${this.form.get('description')?.value}">
+  <meta property="og:image" content="${this.form.get('image')?.value}">
+  <meta property="og:locale" content="${this.form.get('locale')?.value}"/>
 
-      <!-- Twitter Meta Tags -->
-      <meta name="twitter:card" content="summary">
-      <meta property="twitter:domain" content="${this.form.get('url')?.value}">
-      <meta property="twitter:url" content="${this.form.get('url')?.value}">
-      <meta name="twitter:title" content="${this.form.get('title')?.value}">
-      <meta name="twitter:site" content="${this.form.get('site')?.value}">
-      <meta name="twitter:description" content="${this.form.get('description')?.value}">
-      <meta name="twitter:image" content="${this.form.get('image')?.value}">
-      <meta name="twitter:image:alt" content="${this.form.get('imageAlt')?.value}">
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary">
+  <meta property="twitter:domain" content="${this.form.get('url')?.value}">
+  <meta property="twitter:url" content="${this.form.get('url')?.value}">
+  <meta name="twitter:title" content="${this.form.get('title')?.value}">
+  <meta name="twitter:site" content="${this.form.get('site')?.value}">
+  <meta name="twitter:description" content="${this.form.get('description')?.value}">
+  <meta name="twitter:image" content="${this.form.get('image')?.value}">
+  <meta name="twitter:image:alt" content="${this.form.get('imageAlt')?.value}">
 
-      <!-- Meta Tags Generated via https://gustavoquinalha.github.io/seotopper -->
-    `
+  <!-- Meta Tags Generated via https://gustavoquinalha.github.io/seotopper -->
+  `
     });
   }
 
